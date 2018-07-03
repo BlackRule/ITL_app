e3cc74e1955e61a7ab430a1749f26755f4fcac5e
Rules <rulesprog@gmail.com>
2018-07-03 23:01:14 +0200

    родной bootomNavigationView не настраивается А он от китайца https://github.com/ittianyu/BottomNavigationViewEx требует databinding А он "не работает на не-base модулях пока" https://issuetracker.google.com/issues/63814741 поэтому мы вынуждены делать не instantApp а обычную приложуху

---
+++ index/src/main/java/ru/itlservice39/app/index/BaseApplication.java
@@ -0,0 +1,18 @@
+package com.ittianyu.bottomnavigationviewexsample.common.base;
+
+import android.app.Application;
+
+
+/**
+ * Created by yu on 2016/11/24.
+ */
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+
+
+}
