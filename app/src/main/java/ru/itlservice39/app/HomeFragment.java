e4e0503f5b82f21d2689046fe8270f2b00c8c2b7
Rules <rulesprog@gmail.com>
2018-07-07 19:19:07 +0200

    Init TabLayout done #13

--- app/src/main/java/ru/itlservice39/app/HomeFragment.java
+++ app/src/main/java/ru/itlservice39/app/HomeFragment.java
@@ -1,9 +1,14 @@
 package ru.itlservice39.app;
 
+import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,23 +20,56 @@ import ru.itlservice39.app.databinding.FragHomeBinding;
  */
 
 public class HomeFragment extends Fragment {
-    private String title;
-    FragHomeBinding binding;
+	private String title;
+	FragHomeBinding binding;
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // get title
-        title = getArguments().getString("title");
-    }
+	@Override
+	public void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// get title
+		title = getArguments().getString("title");
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = View.inflate(getContext(), R.layout.frag_home, null);
-        // bind view
-//        binding = DataBindingUtil.bind(view);
+	}
+
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		View view = View.inflate(getContext(), R.layout.frag_home, null);
+		// bind view
+		binding = DataBindingUtil.bind(view);
 //        binding.tvTitle.setText(title);
-        return view;
-    }
+		// Получаем ViewPager и устанавливаем в него адаптер
+		ViewPager viewPager = binding.viewpager;
+		viewPager.setAdapter(new SampleFragmentPagerAdapter(getChildFragmentManager()));
+
+		// Передаём ViewPager в TabLayout
+		TabLayout tabLayout = binding.slidingTabs;
+		tabLayout.setupWithViewPager(viewPager);
+		return view;
+	}
+
+	private static class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
+		final int PAGE_COUNT = 3;
+		private String tabTitles[] = new String[]{"Tab1", "Tab2", "Tab3"};
+
+		public SampleFragmentPagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
+
+		@Override
+		public int getCount() {
+			return PAGE_COUNT;
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			return PageFragment.newInstance(position + 1);
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			// генерируем заголовок в зависимости от позиции
+			return tabTitles[position];
+		}
+	}
 }
